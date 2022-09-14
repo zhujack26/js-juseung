274d27f72bd0bea9c9eb1638c688f787aa4a9e0f
unknown <zhujack26@gmail.com>
2022-09-14 22:28:37 +0900

    220914

--- 5356. 의석이의 세로로 말해요/5356.py
+++ 5356. 의석이의 세로로 말해요/5356.py
@@ -1,12 +1,20 @@
 T = int(input())
 for tc in range(1, T+1):
-    arr = [list(map(str, input().split())) for _ in range(5)]
-    max_arr = 0
-    result = []
+    arr = [list(map(str, input())) for _ in range(5)]
+    result = ''
+    # 인덱스 사용방식, 전체(공란포함) 구하고 나중에 빼기, zip 함수 사용 방식
+    max_len = 0
     for i in range(5):
-        if len(*arr[i]) > max_arr:
-            max_arr = len(*arr[i])
-        for j in range(max_arr):
-            result.append(arr[j][i])
-            print(result)
+        if max_len < len(arr[i]):
+            max_len = len(arr[i])
+    for j in range(1, max_len+1):
+        for k in range(5):
+            if j <= len(arr[k]):
+                result += arr[k][j-1]
+            else:
+                continue
+    print(f'#{tc}', end=' ')
+    for i in range(len(result)):
+        print(result[i], end = '')
+    print()
 

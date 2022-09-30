12126e8ab2433cfd21a3c109f85960710f8b51b5
unknown <zhujack26@gmail.com>
2022-09-30 10:01:04 +0900

    0930

--- 보충/4408. 자기 방으로 돌아가기/4408(ing).py
+++ 보충/4408. 자기 방으로 돌아가기/4408(ing).py
@@ -1,11 +1,21 @@
 for tc in range(1, int(input())+1):
     N = int(input())
     board = [0]* 401
-    list = []
+    lst = []
+    count = []
     for i in range(N):
         now, back = map(int, input().split())
         for j in range(now-1, back):
             board[j] += 1
-        if i in board:
-            list.append(i)
-    print(max(list))
+    for i in range(400):
+        if board[i] != board[i+1] :
+            lst.append(board[i])
+    # for i in lst:
+    #     if i != 1 and i != 0:
+    #         count.append(i)
+    print(lst)
+    # print(f'#{tc}', len(count)+1)
+
+    #
+    # print(f'#{tc} {max(board)}')
+    # print(len(board))

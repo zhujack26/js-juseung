67008bde99676f8ae6c13d79c9dcadc6a516fa35
unknown <zhujack26@gmail.com>
2022-10-02 23:37:56 +0900

    1002

--- 보충/12712. 파리퇴치3/12712_solution1.py
+++ 보충/12712. 파리퇴치3/12712_solution1.py
@@ -7,15 +7,26 @@ for tc in range(1, int(input())+1):
     db = [1, -1, -1, 1]
 
     # 상하좌우 + 중앙 합
-    count_max = 0
+    result = 0
     for i in range(N):
         for j in range(N):
-            count = 0
-            for l in range(1, M): #M의 세기 #M 2, 1
+            count = arr[i][j]
+            for l in range(1, M): #M의 세기 M은 2 이상 N 이하
                 for k in range(4):
-                    if i+dx[k]*l < N and j+dy[k]*l < N:
+                    if 0<= i+dx[k]*l < N and 0<= j+dy[k]*l < N:
                         count += arr[i+dx[k]*l][j+dy[k]*l]
-                        if count > count_max:
-                            count_max = count
-    print(count_max)
+            if count > result:
+                result = count
+
     # 대각선 + 중앙 합
+    result2 = 0
+    for i in range(N):
+        for j in range(N):
+            count = arr[i][j]
+            for l in range(1, M):  # M의 세기 #M 2, 1 #M은 2 이상 N 이하
+                for k in range(4):
+                    if 0<= i+da[k]*l < N and 0<= j+db[k]*l < N:
+                        count += arr[i+da[k]*l][j+db[k]*l]
+            if count > result2:
+                result2 = count
+    print(f'#{tc}', max(result,result2))

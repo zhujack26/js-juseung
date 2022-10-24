1b569ff783f787201d229ae9b67bc00318813402
SSAFY <zhujack26@gmail.com>
2022-10-24 17:59:31 +0900

    1024_2

--- js/array.js
+++ js/array.js
@@ -4,3 +4,14 @@ console.log(numbers[0])
 console.log(numbers[-1])
 console.log(numbers.length)
 console.log(numbers[numbers.length-1])
+
+numbers.reverse()
+console.log(numbers)
+
+numbers.push(100)
+console.log(numbers)
+
+numbers.pop()
+console.log(numbers)
+
+//배열 메서드 기초 04:18초
